188ffbc1e50e8e1fd54a2a9c3f478b9682d4585c
Ludo62230 <90885543+ludo62@users.noreply.github.com>
2021-12-09 10:26:34 +0100

    config folder and files

--- server.js
+++ server.js
@@ -1,2 +1,11 @@
 require('dotenv').config();
 const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT;
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+    }
+);
