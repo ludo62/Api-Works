fce0a4aa6e83eb66fb274e0bc783a66cd45534ad
Ludo62230 <90885543+ludo62@users.noreply.github.com>
2021-12-09 13:43:24 +0100

    update routes end test ejs

--- server.js
+++ server.js
@@ -1,11 +1,39 @@
 require('dotenv').config();
 const express = require('express');
+const createHttperrors = require('http-errors');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 const app = express();
+app.use(morgan('dev'));
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
-const port = process.env.PORT;
+app.use('/', require('./routes/index.route'));
+app.use('/auth', require('./routes/auth.route'));
+app.use('/user', require('./routes/user.route'));
 
-app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
-    }
-);
+app.use((req, res, next) => {
+	next(createHttperrors.NotFound());
+});
+
+app.use((error, req, res, next) => {
+	res.status(error.status || 500);
+	res.status(error.status);
+	res.render('error_40x', { error });
+});
+
+const PORT = process.env.PORT;
+
+mongoose
+	.connect(process.env.MONGO_URI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => {
+		console.log('Connected to MongoDB');
+		app.listen(PORT, () => {
+			console.log(`Server is running on port ${PORT}`);
+		});
+	})
+	.catch((err) => console.log(err.message));
