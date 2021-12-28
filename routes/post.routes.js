b27acd3dd5546271a156996e59e27ccd12eef4e9
Ludo62 <fournier.ludovic@orange.fr>
2021-12-28 16:51:09 +0100

    Upload fix

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -5,7 +5,7 @@ const multer = require('multer');
 
 const fileStorageEngine = multer.diskStorage({
 	destination: (req, file, cb) => {
-		cb(null, 'uploads');
+		cb(null, './uploads/');
 	},
 	filename: (req, file, cb) => {
 		cb(null, Date.now() + '--' + file.originalname);
